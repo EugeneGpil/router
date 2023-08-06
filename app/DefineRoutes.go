dfa37b6b862049d3ab9c5f59875cf13cad16cac7
EugeneGpil <gpilligrim@gmail.com>
2023-08-06 05:43:49 +0000

    1

--- app/DefineRoutes.go
+++ app/DefineRoutes.go
@@ -11,23 +11,32 @@ import (
 var mux *http.ServeMux
 
 func DefineRoutes(m *http.ServeMux) {
-	urlRoutesMap := make(map[string][]types.Route)
+	groupedByUrlRoutesWithDifferentHttpMethods := getGroupedByUrlRoutesWithDifferentHttpMethods()
 	mux = m
 
-	for _, currentRoute := range globals.Routes {
-		if _, isset := urlRoutesMap[currentRoute.Url]; !isset {
-			urlRoutesMap[currentRoute.Url] = []types.Route{}
-		}
-		urlRoutesMap[currentRoute.Url] = append(urlRoutesMap[currentRoute.Url], currentRoute)
-	}
-
-	for url, routes := range urlRoutesMap {
+	for url, routes := range groupedByUrlRoutesWithDifferentHttpMethods {
 		url1, url2 := getFormattedUrls.Run(url)
 
 		addHandlers(url1, routes)
 		addHandlers(url2, routes)
 	}
 }
+
+func getGroupedByUrlRoutesWithDifferentHttpMethods() map[string][]types.Route {
+	groupedByUrlRoutesWithDifferentHttpMethods := make(map[string][]types.Route)
+
+	for _, currentRoute := range globals.Routes {
+		if _, isset := groupedByUrlRoutesWithDifferentHttpMethods[currentRoute.Url]; !isset {
+			groupedByUrlRoutesWithDifferentHttpMethods[currentRoute.Url] = []types.Route{}
+		}
+		appended := append(groupedByUrlRoutesWithDifferentHttpMethods[currentRoute.Url], currentRoute)
+		groupedByUrlRoutesWithDifferentHttpMethods[currentRoute.Url] = appended
+	}
+
+	return groupedByUrlRoutesWithDifferentHttpMethods
+}
+
+func defineRouteMap()
 
 func addHandlers(url string, routes []types.Route) {
 	mux.HandleFunc(url, func(writer http.ResponseWriter, request *http.Request) {
