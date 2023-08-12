f5db1b8d813eb2ed9f0ef18a2b84cc2789e7948f
EugeneGpil <gpilligrim@gmail.com>
2023-08-12 04:34:49 +0000

    1

--- app/modules/add/tests/AddRoute/AddRoute_test.go
+++ app/modules/add/tests/AddRoute/AddRoute_test.go
@@ -1,4 +1,4 @@
-package tests
+package AddRoute
 
 import (
 	"bytes"
@@ -10,38 +10,50 @@ import (
 	"github.com/EugeneGpil/router/app/modules/test"
 )
 
-func Test_should_add_route(t *testing.T) {
-	helloMessage := []byte("Hello")
+var helloMessage = []byte("Hello")
+var url = ""
+var tester *testing.T
 
+func Test_should_add_route(t *testing.T) {
+	tester = t
+
+	addRoute()
+
+	assertCallback()
+}
+
+func addRoute() {
 	callback := func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write(helloMessage)
 	}
 
-	url := ""
-
 	add.AddRoute(http.MethodGet, url, callback)
+}
 
+func assertPrimitives() {
 	route := globals.Routes[0]
 
 	if route.Method != http.MethodGet {
-		t.Fatalf(`route.Method = %q, want match for %q`, route.Method, http.MethodGet)
+		tester.Fatalf(`route.Method = %q, want match for %q`, route.Method, http.MethodGet)
 	}
 
 	if route.Url != url {
-		t.Fatalf(`route.Url = %q, want match for %q`, route.Url, url)
+		tester.Fatalf(`route.Url = %q, want match for %q`, route.Url, url)
 	}
 
 	if route.Callback == nil {
-		t.Fatal(`route.Handler is nil`)
+		tester.Fatal(`route.Handler is nil`)
 	}
+}
 
+func assertCallback() {
 	testResponseWriter := test.ResponseWriter{}
 
-	route.Callback(&testResponseWriter, nil)
+	globals.Routes[0].Callback(&testResponseWriter, nil)
 
 	responseMessage := testResponseWriter.GetMessages()[0]
 
 	if bytes.Compare(responseMessage, helloMessage) != 0 {
-		t.Fatalf(`responseMessage = %q, want match for %q`, responseMessage, helloMessage)
+		tester.Fatalf(`responseMessage = %q, want match for %q`, responseMessage, helloMessage)
 	}
 }
