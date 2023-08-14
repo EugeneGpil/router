6352415512cc50eafe911b91776c06d84fa30459
EugeneGpil <gpilligrim@gmail.com>
2023-08-14 06:47:28 +0000

    1

--- app/modules/test/vars/tester/tester.go
+++ app/modules/test/vars/tester/tester.go
@@ -1,9 +1,10 @@
 package tester
 
 import (
-	"bytes"
 	"reflect"
 	"testing"
+
+	"github.com/EugeneGpil/router/app/modules/test/vars/tester/methods"
 )
 
 var t *testing.T
@@ -13,19 +14,7 @@ func SetTester(tester *testing.T) {
 }
 
 func AssertSame(var1 interface{}, var2 interface{}) {
-	if reflect.TypeOf(var1) != reflect.TypeOf(var2) {
-		t.Fatalf(`types do not match %T, %T`, var1, var2)
-	}
-
-	if reflect.TypeOf(var1).String() == "[]uint8" {
-		AssertSame(bytes.Compare(var1.([]byte), var2.([]byte)), 0)
-
-		return
-	}
-
-	if var1 != var2 {
-		t.Fatalf(`var1 = %q, want match for %q`, var1, var2)
-	}
+	methods.AssertSame(var1, var2, t)
 }
 
 func AssertNotNil(var1 interface{}) {
