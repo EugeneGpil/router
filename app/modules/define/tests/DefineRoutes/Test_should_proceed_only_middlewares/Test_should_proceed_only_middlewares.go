ad58274d782a06268252df3679ec05cc232a51ad
EugeneGpil <gpilligrim@gmail.com>
2023-09-02 05:38:39 +0000

    1

---
+++ app/modules/define/tests/DefineRoutes/Test_should_proceed_only_middlewares/Test_should_proceed_only_middlewares.go
@@ -0,0 +1,38 @@
+package DefineRoutes
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/EugeneGpil/router/app/modules/add"
+	"github.com/EugeneGpil/router/app/modules/define/tests/DefineRoutes"
+	"github.com/EugeneGpil/router/app/ship/types"
+	"github.com/EugeneGpil/router/app/ship/utils/tests"
+	"github.com/EugeneGpil/tester"
+)
+
+func Test_should_proceed_only_middlewares(t *testing.T) {
+	tester.SetTester(t)
+
+	addRoute()
+
+	DefineRoutes.AssertWriterMessages([][]byte{
+		DefineRoutes.Middleware1Message,
+		DefineRoutes.Middleware2Message,
+		DefineRoutes.Middleware3Message,
+	})
+}
+
+func addRoute() {
+	callback := tests.GetHttpHandler(DefineRoutes.CallbackMessage)
+
+	middleware1 := tests.GetMiddleware(DefineRoutes.Middleware1Message)
+	middleware2 := tests.GetMiddleware(DefineRoutes.Middleware2Message)
+	middleware3 := tests.GetMiddlewareFalse(DefineRoutes.Middleware3Message)
+
+	add.AddRouteWitMiddlewares(http.MethodGet, DefineRoutes.Url, callback, []types.Middleware{
+		middleware1,
+		middleware2,
+		middleware3,
+	})
+}
